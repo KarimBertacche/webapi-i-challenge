884930b369d63c3d4c820e9b212713af9e4381c0
Karim B <mattiakarim@live.it>
2019-07-08 14:16:01 +0200

    Chore: added methods for all CRUDs listed

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ server.use(express.json());
 
 server.get('/api/users', (req, res) => {
     db.find()
-        .then(data => {
+        .then(user => {
             res.status(200).json({
-                result: data
+                result: user
             })
         })
         .catch(error => {
             res.status(404).json({
-                message: 'error retrieving the data'
+                message: 'error retrieving users'
             })
         });
 });
@@ -23,20 +23,79 @@ server.get('/api/users/:id', (req, res) => {
     const { id } = req.params;
 
     db.findById(id)
-        .then(data => {
-            if(data) {
+        .then(user => {
+            if(user) {
                 res.status(200).json({
-                    result: data
+                    result: user
                 });
             } else {
                 res.status(404).json({
-                    message: 'Id not found'
+                    message: 'User Id not found'
                 });
             }
         })
         .catch(error => {
             res.status(500).json({
-                message: 'error retrieving the data'
+                message: 'error retrieving the user'
+            });
+        });
+});
+
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+
+    db.insert(newUser)
+        .then(user => {
+            res.status(201).json({
+                result: user
+            })
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'error creating new user'
+            })
+        });
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.remove(id)
+        .then(deleted => {
+            if(deleted) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({
+                    message: 'User Id not found'
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'error deleting user'
+            })
+        });
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    db.update(id, changes)
+        .then(updated => {
+            if(updated) {
+                res.status(200).json({
+                    result: updated
+                });
+            } else {
+                res.status(404).json({
+                    message: 'User Id not found'
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'error updating user'
             });
         });
 });
