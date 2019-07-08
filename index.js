6f81b93a6d5df9df6525501e95f055dd5116e60c
Karim B <mattiakarim@live.it>
2019-07-08 13:48:02 +0200

    Chore: Started project and added initial boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express');
+const server = express();
+const db = require('./data/db');
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(data => {
+            res.status(200).json({
+                result: data
+            })
+        })
+        .catch(error => {
+            res.status(404).json({
+                message: 'error retrieving data'
+            })
+        });
+});
+
+server.listen(3000, () => {
+    console.log('** Listening on port 3K **');
+});
